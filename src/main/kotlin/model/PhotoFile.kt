8b400baa4a3df8df1b371632f0672ba04cd76343
nmorcab2106 <amorac@iesrafaelalberti.es>
2026-06-16 12:33:10 +0200

    PhotoFile.kt corregido

--- src/main/kotlin/model/PhotoFile.kt
+++ src/main/kotlin/model/PhotoFile.kt
@@ -1,6 +1,5 @@
 package org.iesra.model
 
-abstract class PhotoFile {
-    abstract val originalName: String
+abstract class PhotoFile(val originalName: String) {
     abstract var orderKey: String
 }
