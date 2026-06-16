e9754e2c84e9ba043f65b832dc033811aef6f59b
nmorcab2106 <amorac@iesrafaelalberti.es>
2026-06-16 09:24:34 +0200

    Creación de InputFileReader.kt

--- src/main/kotlin/io/InputFileReader.kt
+++ src/main/kotlin/io/InputFileReader.kt
@@ -1,4 +1,19 @@
 package org.iesra.io
 
+import org.iesra.model.TripInput
+import java.io.File
+import java.nio.file.Path
+
 class InputFileReader {
+    fun read(inputPath: Path): TripInput? {
+        try {
+            val ficheroLeido = File("$inputPath").readText()
+            val fichero = TripInput(ficheroLeido[0].toString(), listOf(ficheroLeido[1].toString()), listOf(ficheroLeido[2].toString()))
+            return fichero
+        }catch (e: Exception){
+            println("Error: No se ha podido leer el fichero")
+            println(e)
+            return null
+        }
+    }
 }
