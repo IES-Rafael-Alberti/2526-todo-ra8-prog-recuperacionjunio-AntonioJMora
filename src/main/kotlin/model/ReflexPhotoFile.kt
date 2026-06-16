88a10c6cf16fd952029ba03006deed4899e919af
nmorcab2106 <amorac@iesrafaelalberti.es>
2026-06-16 12:38:07 +0200

    Añadido para que separen tambien por .

--- src/main/kotlin/model/ReflexPhotoFile.kt
+++ src/main/kotlin/model/ReflexPhotoFile.kt
@@ -2,7 +2,7 @@ package org.iesra.model
 
 class ReflexPhotoFile(originalName: String) : PhotoFile(originalName) {
     val partes1 = originalName.split("P")
-    val partes2 = partes1[1].split("_")
+    val partes2 = partes1[1].split("_", ".")
     val partesAno = partes2[0].split("DD", "MM", "YY")
     val ordenadas = "20" + partesAno[2] + partesAno[1] + partes1[0]
 
