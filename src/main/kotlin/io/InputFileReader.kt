b0a49c17b1eab3a1da72eefe1775e7a28bb6c5b0
nmorcab2106 <amorac@iesrafaelalberti.es>
2026-06-16 10:08:16 +0200

    Correcciones en InputFileReader.kt

--- src/main/kotlin/io/InputFileReader.kt
+++ src/main/kotlin/io/InputFileReader.kt
@@ -7,8 +7,12 @@ import java.nio.file.Path
 class InputFileReader {
     fun read(inputPath: Path): TripInput? {
         try {
-            val ficheroLeido = File("$inputPath").readText()
-            val fichero = TripInput(ficheroLeido[0].toString(), listOf(ficheroLeido[1].toString()), listOf(ficheroLeido[2].toString()))
+            val ficheroLeido = File("$inputPath").readLines()
+            val nombresSmart = ficheroLeido[1].split(" ")
+            val nombresReflex = ficheroLeido[2].split(" ")
+            val fichero = TripInput(ficheroLeido[0], nombresSmart, nombresReflex)
+
+            // TODO: hacer validaciones de que sean 3 lineas, que la primera linea sea solo una palabra
             return fichero
         }catch (e: Exception){
             println("Error: No se ha podido leer el fichero")
